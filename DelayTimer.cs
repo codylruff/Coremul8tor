10a9bc3b1ecb25815eb2381ea5030b3502c91add
codylruff <39601024+codylruff@users.noreply.github.com>
2019-01-18 08:20:11 -0500

    Update DelayTimer.cs

--- DelayTimer.cs
+++ DelayTimer.cs
@@ -6,5 +6,17 @@ namespace CHIP_8_Emulator
     {
         // TODO: Construct class
         private const float FREQUENCY_HZ = 60.0;
+
+        //TODO: use asnyc to create delay cycle
+        asnyc
+        {
+            while(cycleIsActive)
+            {
+                for(int i=0; i=MaxTicks; i++)
+                {
+
+                }
+            }
+        }
     }
 }
