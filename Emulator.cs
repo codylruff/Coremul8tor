458f41bf94b025849c2f9413c4c5a36959389841
codylruff <39601024+codylruff@users.noreply.github.com>
2019-03-30 23:58:38 -0400

    Fixed dictionary

--- Emulator.cs
+++ Emulator.cs
@@ -101,21 +101,6 @@ namespace CHIP_8_Emulator
         {
            opcodes = new Dictionary<byte, Action<OpCodeData>>
            {
-                { 0x0, ClearOrReturn }, // There are two instructions starting with a 0 nibble, didn't seem worth its own dictionary
-                { 0x1, Jump },
-                { 0x2, CallSubroutine },
-                { 0x3, SkipIfXEqual },
-                { 0x4, SkipIfXNotEqual },
-                { 0x5, SkipIfXEqualY },
-                { 0x6, SetX },
-                { 0x7, AddX },
-                { 0x8, Arithmetic },
-                { 0x9, SkipIfXNotEqualY },
-                { 0xA, SetI },
-                { 0xB, JumpWithOffset },
-                { 0xC, Rnd },
-                { 0xD, DrawSprite },
-                { 0xE, SkipOnKey },
                 {0x0,ClearOrReturn},
                 {0x1,Jump},
                 {0x2,CallSubroutine},
