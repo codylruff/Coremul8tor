7d7b069ea6a4f527baea94ae9bd9584b07e9b20e
codylruff <39601024+codylruff@users.noreply.github.com>
2019-08-25 11:15:03 -0400

    Update chip8.h

--- chip8.h
+++ chip8.h
@@ -12,19 +12,19 @@ private:
     std::map<char,void (*op)> operations;
     void LoadOperations()
     {
-                {0x0,ClearOrReturn},
-                {0x1,Jump},
-                {0x2,CallSubroutine},
-                {0x3,SkipIfXEqual},
-                {0x4,SkipIfXNotEqual},
-                {0x5,SkipIfXEqualY},
-                {0x6,SetX},
-                {0x7,AddX},
-                {0x8,Arithmetic},
-                {0x9,SkipIfXNotEqualY},
-                {0xA,SetI},
-                {0xB,JumpWithOffSet},
-                {0xC,Rnd},
+                operations[0x0] = ClearOrReturn
+                operations[0x1] = Jump
+                operations[0x2] = CallSubroutine
+                operations[0x3] = SkipIfXEqual
+                operations[0x4] = SkipIfXNotEqual
+                operations[0x5] = SkipIfXEqualY
+                operations[0x6] = SetX
+                operations[0x7] = AddX
+                operations[0x8] = Arithmetic
+                operations[0x9] = SkipIfXNotEqualY
+                operations[0xA] = SetI
+                operations[0xB] = JumpWithOffSet
+                operations[0xC] =Rnd},
                 {0xD,DrawSprite},
                 {0xE,SkipOnKey},
                 {0xF,Execute_0xF}
