e3ed4f49a1660e0577bc18882ef01b9755e531f2
codylruff <39601024+codylruff@users.noreply.github.com>
2019-08-25 22:16:36 -0400

    Update chip8.h

--- chip8.h
+++ chip8.h
@@ -9,25 +9,25 @@ private:
         char NN;
         char X, Y, N;
      };
-    std::map<char,void (*op)> operations;
+    std::map<char,void (*op)(void)> operations;
     void LoadOperations()
     {
-                operations[0x0] = ClearOrReturn
-                operations[0x1] = Jump
-                operations[0x2] = CallSubroutine
-                operations[0x3] = SkipIfXEqual
-                operations[0x4] = SkipIfXNotEqual
-                operations[0x5] = SkipIfXEqualY
-                operations[0x6] = SetX
-                operations[0x7] = AddX
-                operations[0x8] = Arithmetic
-                operations[0x9] = SkipIfXNotEqualY
-                operations[0xA] = SetI
-                operations[0xB] = JumpWithOffSet
-                operations[0xC] =Rnd},
-                {0xD,DrawSprite},
-                {0xE,SkipOnKey},
-                {0xF,Execute_0xF}
+                operations[0x0] = &ClearOrReturn;
+                operations[0x1] = &Jump;
+                operations[0x2] = &CallSubroutine;
+                operations[0x3] = &SkipIfXEqual;
+                operations[0x4] = &SkipIfXNotEqual;
+                operations[0x5] = &SkipIfXEqualY;
+                operations[0x6] = &SetX;
+                operations[0x7] = &AddX;
+                operations[0x8] = &Arithmetic;
+                operations[0x9] = &SkipIfXNotEqualY;
+                operations[0xA] = &SetI;
+                operations[0xB] = &JumpWithOffSet;
+                operations[0xC] = &Rnd;
+                operations[0xD] = &DrawSprite;
+                operations[0xE] = &SkipOnKey;
+                operations[0xF] = &Execute_0xF;
     };
 public:
    
